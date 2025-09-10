1dc883bdcf397e33afd20181d9b1cec731126115
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2025-09-10 21:33:53 +0800

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>My App page</div>
+  )
+}
+
+export default App
---
+++ server-api/src/main/java/com/kenwrickespinosa/server_api/ServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.kenwrickespinosa.server_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApiApplication.class, args);
+	}
+
+}
